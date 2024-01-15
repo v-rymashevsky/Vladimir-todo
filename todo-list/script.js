7861c09561a80d9094d2f6d403a47e9256095c07
v-rymashevsky <rymashevskiy.v@gmail.com>
2024-01-16 00:13:32 +0400

    header v1(flexbox placeholder)

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,4 +1,70 @@
-const root = document.getElementById('root'); 
+const root = document.getElementById('root');
+
+const header = document.createElement('header');
+root.append(header);
+
+const headerTop = document.createElement('div'); 
+headerTop.className = 'header-top';
+
+const headerBottom = document.createElement('div'); 
+headerBottom.className = 'header-bottom';
 
 
-console.log(document)
+//button
+function createButton(text, type, className) {
+    const button = document.createElement('button');
+    button.innerText = text;
+    button.className = className;
+    button.type = type;
+    return button
+}
+
+//input
+
+function createInput(text, type, className) {
+    const input = document.createElement('input');
+    input.placeholder = text;
+    input.className = className;
+    input.type = type;
+    return input
+}
+
+function createCounter ( text, type, className) { 
+
+    const counter = document.createElement('div');
+    counter.innerText = text; 
+    counter.className = className; 
+    counter.type = type; 
+    return counter
+}
+
+const deleteAllButton = createButton('Delete All', 'button', 'button');
+const deleteLastButton = createButton('Delete Last', 'button', 'button'); 
+const addButton = createButton('Add', 'button', 'button'); 
+const showAllButton = createButton('Show All', 'button', 'button'); 
+const showCompletedButton = createButton('Show Completed', 'button', 'button'); 
+
+const toDoInput = createInput('Ented todo...', 'input', 'input');
+const searchInput = createInput('Search...', 'input', 'input'); 
+
+const counterAll = createCounter('All: 2','div','counter')
+const counterCompleted = createCounter('Completed: 1','div','counter')
+
+
+
+header.append(headerTop, headerBottom)
+
+headerTop.append(deleteAllButton, deleteLastButton, toDoInput, addButton);
+headerBottom.append(counterAll, counterCompleted, showAllButton, showCompletedButton, searchInput);
+
+
+
+
+
+
+
+const taskContainer = document.createElement('div')
+taskContainer.className = 'task-container';
+
+
+
